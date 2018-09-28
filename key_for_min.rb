125909dc08829f0bde88244ca2a15308218838b2
larryvsdata <ermanbekaroglu@gmail.com>
2018-09-28 14:26:34 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,9 @@
 
 def key_for_min_value(name_hash)
 
-keyList=name_hash.keys
-valueList=name_hash.values
 
-minVal=valueList.min
+
+minVal=100000000
 
 
 
@@ -14,10 +13,7 @@ if name_hash.empty?
   nil
   else
 
-    name_hash=[name_hash.min_by{|k, v| v}].to_h
-        keyList=name_hash.keys
-valueList=name_hash.values
-name_hash.keys[0]
+
 end
 
 end
