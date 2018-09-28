01da25431859e971cc01432335b4aba273f03ff3
larryvsdata <ermanbekaroglu@gmail.com>
2018-09-28 14:28:34 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,16 +6,21 @@ def key_for_min_value(name_hash)
 
 
 minVal=100000000
-
+minName=""
 
 
 if name_hash.empty?
   nil
   else
-
-
+name_hash.collect do |name, num|
+    if num<minVal
+      minName=name
+      minVal=num
+    end
 end
 
+end
+minVal
 end
 
 
