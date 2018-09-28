f10c7eccd2d31f3f9d55f44066d5b98399e12788
larryvsdata <ermanbekaroglu@gmail.com>
2018-09-28 14:10:53 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,17 +5,6 @@ def key_for_min_value(name_hash)
 
 
 
-minVal=a.min_by {|k,v| v }
- 
-hKeys=name_hash.keys
-hValues=name_hash.values
-
-hValues.each_with_index { |item, index|
-  if item==minVal
-    return hKeys[index]
-  end
-}
-
 end
 
 
